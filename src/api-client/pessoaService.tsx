a286e308c624d51dcee855e66ba8929e768b97b7
poully branco <poullybranco@yahoo.com.br>
2024-04-01 20:58:55 -0300

    criação pessoa

--- src/api-client/pessoaService.tsx
+++ src/api-client/pessoaService.tsx
@@ -1,13 +1,16 @@
 import axios from 'axios';
+import { QueryClient } from 'react-query';
 
 const API_BASE_URL = '/api/pessoa';
+
 interface Pessoa {
   id: number;
   nome: string;
   idade: number;
+  cidadeId: number;
 }
 
-export async function getPessoas(): Promise<Pessoa[]> {
+export async function getPessoas(queryClient: QueryClient): Promise<Pessoa[]> {
   try {
     const response = await axios.get(`${API_BASE_URL}/pessoas`);
     return response.data;
@@ -17,9 +20,13 @@ export async function getPessoas(): Promise<Pessoa[]> {
   }
 }
 
-export async function createPessoa(pessoa: Pessoa): Promise<Pessoa> {
+export async function createPessoa(
+  pessoa: Omit<Pessoa, 'id'>, // Remova o campo 'id' ao criar uma nova pessoa
+  queryClient: QueryClient,
+): Promise<Pessoa> {
   try {
     const response = await axios.post(`${API_BASE_URL}/pessoas`, pessoa);
+    queryClient.invalidateQueries('pessoas');
     return response.data;
   } catch (error) {
     console.error('Erro ao criar pessoa:', error);
@@ -27,9 +34,10 @@ export async function createPessoa(pessoa: Pessoa): Promise<Pessoa> {
   }
 }
 
-export async function deletePessoa(id: number): Promise<void> {
+export async function deletePessoa(id: number, queryClient: QueryClient): Promise<void> {
   try {
     await axios.delete(`${API_BASE_URL}/pessoas/${id}`);
+    queryClient.invalidateQueries('pessoas');
   } catch (error) {
     console.error(`Erro ao deletar pessoa com ID ${id}:`, error);
     throw error;
--- src/pages/pessoaCreate.tsx
+++ src/pages/pessoaCreate.tsx
@@ -1,24 +1,33 @@
 import { useState } from 'react';
 import { Box, Button, Text, TextInput } from '@mantine/core';
-import { useMutation, QueryClient, QueryClientProvider } from 'react-query'; // Importe o QueryClient e o QueryClientProvider
-import { createPessoa } from '@/src/api-client/pessoaService';
+import { useMutation, QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import { createPessoa } from '@/src/api-client/pessoaService'; 
+import { getCidades } from '@/src/api-client/cidadeService'; 
 
-const queryClient = new QueryClient(); // Crie uma instância do QueryClient
+// Criar uma instância do QueryClient
+const queryClient = new QueryClient();
 
 export default function PessoaCreate() {
   const [nome, setNome] = useState('');
+  const [cidade, setCidade] = useState('');
+  const { data: cidades } = useQuery('cidades', getCidades);
+
   const { mutate } = useMutation(createPessoa, {
     onSuccess: () => {
-      queryClient.invalidateQueries('pessoas');
       setNome('');
+      setCidade('');
+      // Ao criar com sucesso, invalidar a consulta para recarregar os dados
+      queryClient.invalidateQueries('pessoas');
     },
     onError: (error) => {
       console.error('Erro ao criar pessoa:', error.message);
     },
+    // Fornecer o QueryClient criado para o hook useMutation
+    queryClient,
   });
 
   const handleCreatePessoa = () => {
-    mutate({ nome });
+    mutate({ nome, cidade });
   };
 
   return (
@@ -29,16 +38,13 @@ export default function PessoaCreate() {
         value={nome}
         onChange={(event) => setNome(event.target.value)}
       />
+      <TextInput
+        label="Cidade"
+        value={cidade}
+        onChange={(event) => setCidade(event.target.value)}
+        data={cidades?.map((cidade) => cidade.nome)}
+      />
       <Button onClick={handleCreatePessoa}>Adicionar</Button>
     </Box>
-  );
-}
-
-// Envolve o componente PessoaCreate em QueryClientProvider e fornece o QueryClient criado
-export function PessoaCreateWithQueryClientProvider() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <PessoaCreate />
-    </QueryClientProvider>
   );
 }
