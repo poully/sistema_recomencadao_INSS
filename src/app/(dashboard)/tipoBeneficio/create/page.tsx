6946e6fb59f5eb94cb72a7cdef8fc1d77abd5d39
poully <poullybranco@yahoo.com.br>
2024-05-03 12:02:49 -0300

    correções e criação do Tipo

--- src/app/(dashboard)/tipoBeneficio/create/page.tsx
+++ src/app/(dashboard)/tipoBeneficio/create/page.tsx
@@ -1,10 +1,7 @@
 'use client'
 import { useAxiosClient } from '@/src/api-client/getAxiosClient';
-import { Box, Button, Text, TextInput } from '@mantine/core';
-import { useForm } from '@mantine/form';
 import { Tipo } from '@prisma/client';
 import { useRouter } from 'next/navigation';
-import { useTransition } from 'react';
 import { toast } from 'react-toastify';
 
 type TipoForm = Omit<Tipo, "id">;
@@ -12,37 +9,19 @@ type TipoForm = Omit<Tipo, "id">;
 export default function TipoCreate() {
     const axios = useAxiosClient();
     const router = useRouter();
-    const form = useForm<TipoForm>({
-        initialValues: {
-            nome: '',
-        },
-    });
-    console.log(form);
 
-    const [isPending, startTransition] = useTransition();
-    const handleSubmit = (values: TipoForm) => {
-        startTransition(async () => {
-            try {
-                const response = await axios.post("/tipoBeneficio", values);
-                toast.success("Inserido com sucesso.");
-                router.push("/tipoBeneficio");
-                console.log(response);
-            } catch (e) {
-                toast.error("Erro ao adicionar um novo tipo de beneficio");
-            }
-        });
+    const onSubmit = async (values: TipoForm) => {
+        try {
+
+            const response = await axios.post("/pessoas", input);
+            toast.success("Inserido com sucesso.");
+            router.push("/pessoa");
+        } catch (e) {
+            const error = e as AxiosError;
+            // @ts-expect-error
+            toast.error(error.response?.data?.error!);
+        }
     }
-    return (
-        <Box>
-            <Text variant="h1">Adicionar tipo</Text>
-            <form onSubmit={form.onSubmit(handleSubmit)}>
-                <TextInput
-                    label="Nome do tipo beneficio"
-                    {...form.getInputProps('nome')}
-                />
+    return <PessoaForm onSubmit={onSubmit} title="Adicionar pessoa" />
 
-                <Button type="submit" loading={isPending}>Adicionar</Button>
-            </form>
-        </Box>
-    );
 }
